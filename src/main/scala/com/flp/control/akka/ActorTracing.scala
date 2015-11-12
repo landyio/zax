52572738c708726e67cebf950de019ade03545f3
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-11-12 22:30:35 +0300

    (INF): Revisited tracing-actor-facade

--- src/main/scala/com/flp/control/akka/ActorTracing.scala
+++ src/main/scala/com/flp/control/akka/ActorTracing.scala
@@ -1,44 +1,46 @@
 package com.flp.control.akka
 
 import akka.actor.{Actor, ActorLogging}
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.ConfigFactory
 
-trait ActorTracing extends ActorLogging {
-  this: Actor =>
+trait ActorTracing extends ActorLogging { this: Actor =>
+
+  import ActorTracing.TRACE_ASCRIPTION
 
   private val enabled: Boolean = log.isInfoEnabled && ActorTracing.enabled
 
-  protected def trace(receive: Receive): Receive = {
-    return trace("")(receive)
-  }
+  protected def trace(receive: Receive): Receive = trace(TRACE_ASCRIPTION)(receive)
 
-  protected def trace(prefix: String)(receive: Receive): Receive = {
-    if (!enabled) { return receive }
+  protected def trace(prefix: String)(receive: Receive): Receive =
+    if (!enabled)
+      receive
+    else
+      new Receive {
 
-    return new Receive {
-
-      override def isDefinedAt(o: Any): Boolean = {
-        val definedAt: Boolean = receive.isDefinedAt(o)
-        if (!definedAt) {
-          log.info(s"${prefix}${String.valueOf(o)}::undefined (${o.getClass().getName()}})")
+        override def isDefinedAt(o: Any): Boolean = {
+          val definedAt: Boolean = receive.isDefinedAt(o)
+          if (!definedAt) {
+            log.info(s"[$prefix] '${self.path}' is undefined at: ${String.valueOf(o)}::undefined (${o.getClass.getName}})")
+          }
+          definedAt
         }
-        return definedAt
-      }
 
-      override def apply(o: Any): Unit = {
-        log.info(s"${prefix}${String.valueOf(o)}")
-        receive.apply(o)
-      }
+        override def apply(o: Any): Unit = {
+          log.info(s"[$prefix] '${self.path}' received: ${String.valueOf(o)}")
+          receive.apply(o)
+        }
 
-    }
-  }
+      }
 
 }
 
 private object ActorTracing {
 
-  private val param: String = "flp.akka.trace"
-  private val config: Config = ConfigFactory.load()
-  private val enabled: Boolean = config.hasPath(param) && config.getBoolean(param)
+  private val TRACE_ASCRIPTION: String = "TRC"
+
+  private val param   = "flp.akka.trace"
+  private val config  = ConfigFactory.load()
+
+  private val enabled = config.hasPath(param) && config.getBoolean(param)
 
 }
