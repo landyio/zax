e1720046e5e8a208b5879455d17ba149a7e970d0
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-11-15 16:08:21 +0300

    (MOD): Revisited the whole application model;        Merged `UserData.scala` into `model/App.scala`

--- src/main/scala/com/flp/control/model/App.scala
+++ src/main/scala/com/flp/control/model/App.scala
@@ -1,12 +1,25 @@
 package com.flp.control.model
 
 
+/**
+  * Goal
+  */
 trait Goal {
   type Type = Boolean
 }
 
+
+/**
+  * User identity
+  *
+  * @param params parameters specifiying particular user-identity
+  */
 case class UserIdentity(params: UserIdentity.Params) {
   def ++(ps: UserIdentity.Params) = UserIdentity(params ++ ps)
+
+  def toFeatures(descriptors: Seq[UserDataDescriptor]): Vector[Double] =
+    descriptors .map { d => d.hash(params.get(d.name)).toDouble }
+                .toVector
 }
 
 object UserIdentity {
@@ -16,10 +29,31 @@ object UserIdentity {
   val empty = new UserIdentity(Map())
 }
 
+/**
+  * Variation
+  *
+  * @param id unique id of the variation
+  */
 case class Variation(id: Variation.Id)
 
 object Variation {
   type Id = String
 
   val sentinel = new Variation("-1")
+}
+
+
+/**
+  * User's identity detail descriptor
+  *
+  * @param name name of the detail
+  * @param hash optional hash-function
+  */
+case class UserDataDescriptor(
+  name: String,
+  hash: (Option[String] => Int) = UserDataDescriptor.defaultHash
+)
+
+object UserDataDescriptor {
+  protected val defaultHash: Option[String] => Int = o => o.map { _.hashCode() }.getOrElse(0)
 }
