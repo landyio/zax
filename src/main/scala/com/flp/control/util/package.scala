70b450df33fb1432678c0a7c1650c6b09442f8c8
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-11-19 03:18:25 +0300

    (UTIL): Missing utilities

--- src/main/scala/com/flp/control/util/package.scala
+++ src/main/scala/com/flp/control/util/package.scala
@@ -1,7 +1,13 @@
 package com.flp.control
 
+import scala.language.implicitConversions
+
 package object util {
 
   implicit def boolean2Int(b: Boolean): Int = if (b) 1 else 0
 
+  object Identity {
+    @inline def partial[T](): PartialFunction[T, T] = { case x => x }
+    @inline def total[T](): (T) => T = { x => x }
+  }
 }
