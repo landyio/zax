32e99f1a47f025c2c7b2a88cc84d24cc22042001
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-12-02 01:53:45 +0300

    (MISC): Tidying up

--- src/main/scala/io/landy/app/util/package.scala
+++ src/main/scala/io/landy/app/util/package.scala
@@ -6,6 +6,9 @@ package object util {
 
   implicit def boolean2Int(b: Boolean): Int = if (b) 1 else 0
 
+  /**
+    * Reflection
+    */
   object Reflect {
     def moduleFrom[T](sym: scala.reflect.runtime.universe.Symbol): T = {
       val mod = sym.owner.typeSignature.member(sym.name.toTermName)
@@ -13,6 +16,9 @@ package object util {
     }
   }
 
+  /**
+    * Functor-specific
+    */
   object Identity {
     @inline def partial[T](): PartialFunction[T, T] = { case x => x }
     @inline def total[T](): (T) => T = { x => x }
